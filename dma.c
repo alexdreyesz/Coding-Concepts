71a1a52521d87491128db74e250d1e91b7a1fd09
alexdreyesz <alexdreyesz@gmail.com>
2024-07-26 18:14:19 -0400

    DMA Array

--- dma.c
+++ dma.c
@@ -17,8 +17,14 @@ node* dma_struct() {
 }
 
 // DMA For An Array
-void dma_array() {
+int* dma_array() {
+    int *array = (int*)malloc(10 * sizeof(int));
 
+    for(int i = 0; i < 10; i++) {
+        array[i] = i * 2; 
+    }
+
+    return array;
 } 
 
 // DMA For A 2D Array
@@ -32,11 +38,17 @@ void dma_array_of_arrays() {
 } 
 
 int main() {
-
+    // DMA For A struct
     node *root = dma_struct();
     printf("\nNode: %d\n\n", root->data);
 
-    
+    // DMA For An Array
+    int *array = dma_array();
+    for(int i = 0; i < 10; i++) {
+        printf("Array[%d]: %d\n", i, array[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
