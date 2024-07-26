1687b3af4db675ee211272833a8e9c2e9bf867e8
alexdreyesz <alexdreyesz@gmail.com>
2024-07-26 17:44:48 -0400

    First Commit

---
+++ dma.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Struct To Be DMA
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+// DMA For A struct
+void dma_struct() {
+
+}
+
+// DMA For An Array
+void dma_array() {
+
+} 
+
+// DMA For A 2D Array
+void dma_2d_array() {
+
+} 
+
+// DMA For An Array Of Arrays
+void dma_array_of_arrays() {
+
+} 
+
+int main() {
+
+
+
+    return 0;
+}
