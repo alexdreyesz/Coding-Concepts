f5e4eab5b4fafa570d099c49679088513a7d8595
alexdreyesz <alexdreyesz@gmail.com>
2024-08-11 12:15:36 -0400

    Selection Sort

--- selection.c
+++ selection.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
+// Selection Sort
 void selection_sort(int array[], int size) {
     int temporary = 0;
     int swap_index = 0;
 
     for(int i = 0; i < size - 1; i++) {
+        // Reset Swap Index To Current Index
         swap_index = i;
 
+        // Search For A New Minimum
         for(int j = i + 1; j < size; j++) {
+            // New Minimum Found Update Swap Index
             if(array[swap_index] > array[j]) {
                 swap_index = j;
             }
         }
 
-        // Swap The Found Minimun Element With The First Element
-        if(swap_index != i) {
-            temporary = array[i];
-            array[i] = array[swap_index];
-            array[swap_index] = temporary;
-        }
+        // Swap The Found Minimun Element With The Current Element
+        temporary = array[i];
+        array[i] = array[swap_index];
+        array[swap_index] = temporary;
     }
 }
 
 int main() {
-
     int array[10] = {3,1,4,7,2,5,8,9,6,10};
     int size = 10;
 
