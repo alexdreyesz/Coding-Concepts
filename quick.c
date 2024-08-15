ed6c8943c184df51b39c86dcf4a4d9c66041b21e
alexdreyesz <alexdreyesz@gmail.com>
2024-08-14 23:16:05 -0400

    Quick Sort

--- quick.c
+++ quick.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
+// Quick Sort 
 void quick_sort(int array[], int size) {
     
 }
 
 int main() {
-
-    int array[10] = {3,1,4,7,2,5,8,9,6,10};
-    int size = 10;
+    int array[11] = {3,1,4,11,7,2,5,8,9,6,10};
+    int size = 11;
 
     // Print Unsorted Array
     for(int i = 0; i < size; i++) {
